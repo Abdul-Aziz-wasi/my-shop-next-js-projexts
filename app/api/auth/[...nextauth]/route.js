38b888f73bd674ff65893cf7aa7efc72d5aa246a
Abdul Aziz <abdulaziz758015@gmail.com>
2025-08-28 10:39:59 +0600

    fixed the signIn method

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -19,26 +19,30 @@ const handler = NextAuth({
     },
     async authorize(credentials, req) {
       console.log("credentials",credentials)
+      const user ={ id: 1, name: 'J Smith', email: 'jsmith@example.com' }
       // You need to provide your own logic here that takes the credentials
       // submitted and returns either a object representing a user or value
       // that is false/null if the credentials are invalid.
       // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
       // You can also use the `req` object to obtain additional parameters
       // (i.e., the request IP address)
-      const res = await fetch("http://localhost:3000/api/login", {
-        method: 'POST',
-        body: JSON.stringify(credentials),
-        headers: { "Content-Type": "application/json" }
-      })
-      const user = await res.json()
+      // const res = await fetch("http://localhost:3000/api/login", {
+      //   method: 'POST',
+      //   body: JSON.stringify(credentials),
+      //   headers: { "Content-Type": "application/json" }
+      // })
+      // const user = await res.json()
+      
 
       // If no error and we have user data, return it
-      if (res.ok && user) {
+      if (user) {
         return user
         
-      }
+      }else
       // Return null if user data could not be retrieved
-      return null
+     {
+       return null
+     }
     }
     
   })
